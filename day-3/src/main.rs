9487bd92e7bcbffed47ee4899ccd618d99db6dde
Ostap Tymchenko <ostap.tymchenko@gmail.com>
2022-12-07 22:07:53 -0500

    finished day 3 part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,13 +1,11 @@
-use std::collections::HashSet;
 use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
-use std::str::Chars;
 
 fn open_file() -> String {
     // Create a path to the desired file
-    let path = Path::new("src/dummy-data.txt");
+    let path = Path::new("src/data.txt");
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -32,24 +30,36 @@ pub fn main() {
 
     let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 
+    let mut total_priority = 0;
     for lines in file.lines() {
+        // splits og string in two, creating first and second
         let len = lines.len();
         let first = &lines[0..len / 2];
         let second = &lines[len /2 ..];
 
-        let first_btreeset = BTreeSet::from_iter(first.chars());
+        // turns first and second into a btreeset
         let second_btreeset = BTreeSet::from_iter(second.chars());
 
+        // defines the common charachter between first and second
         let mut common_char = '_';
-
         for char in first.chars() {
             if second_btreeset.contains(&char) {
                 common_char = char;
             } 
         }
 
-        let priority = alphabet.find(common_char);
-        // let priority = priority + 1;
-        dbg!(priority);
+        let priority_buffer= alphabet.find(common_char);
+        // let priority_buffer= priority_buffer+ 1;
+
+        let priority_buffer= match priority_buffer{
+            Some(val) => val,
+            None => {
+                panic!("panic! on a `None` value");
+            }, 
+        };
+        let priority_buffer = priority_buffer+ 1;
+        total_priority += priority_buffer;
     }   
+
+    dbg!(total_priority);
 }
