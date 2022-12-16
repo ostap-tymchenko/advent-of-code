3bfe3c35eeb24932d3a1c0cf7d73213d7d0bb0c4
Ostap Tymchenko <ostap.tymchenko@gmail.com>
2022-12-15 21:31:57 -0500

    end of day progress save

--- day-5/src/part_2.rs
+++ day-5/src/part_2.rs
@@ -1,11 +1,21 @@
-use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use couleur::{Couleur, Colors};
 
-fn open_file() -> String {
+//         [H]     [W] [B]            
+//     [D] [B]     [L] [G] [N]        
+// [P] [J] [T]     [M] [R] [D]        
+// [V] [F] [V]     [F] [Z] [B]     [C]
+// [Z] [V] [S]     [G] [H] [C] [Q] [R]
+// [W] [W] [L] [J] [B] [V] [P] [B] [Z]
+// [D] [S] [M] [S] [Z] [W] [J] [T] [G]
+// [T] [L] [Z] [R] [C] [Q] [V] [P] [H]
+//
+// 1   2   3   4   5   6   7   8   9 
+
+fn open_file(path: &Path) -> String {
     // Create a path to the desired file
-    let path = Path::new("src/data.txt");
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -18,17 +28,163 @@ fn open_file() -> String {
     let mut data = String::new();
     match file.read_to_string(&mut data) {
         Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => print!("{} contains: data\n", display),
+        Ok(_) => print!(""),
     }
 
     data
 }
 
-
 pub fn main() {
-    let file = open_file();
+    let data_path = Path::new("src/dummy-data.txt");
+    let data = open_file(data_path);
 
-    for line in file.lines() {
-    
+    let chart_path = Path::new("src/chart.txt");
+    let chart = open_file(chart_path);
+
+    // println!("{chart}");
+    for line in chart.lines() {
+        if line == "" {
+            // nothing
+        } else {
+            println!("{line}")
+        }
     }
-}        
+    println!();
+
+    let mut chart_vec = vec![];
+
+    for line in chart.lines() {
+        chart_vec.push(line.to_string())
+    }
+
+    let chart_vec_old = &chart_vec.to_owned();
+
+    for line in data.lines() {
+        if line != "" {
+
+            let i: Vec<&str> = line.split("-").collect();
+
+            let mut move_times:usize = i[0].parse().unwrap();
+            let crate_from:usize = i[1].parse().unwrap();
+            let crate_to:usize = i[2].parse().unwrap();
+            
+            let crate_from = crate_from -1;
+            let crate_to = crate_to -1;
+
+            // these 4 lines make sure that there are enough crates to move, and prevents a crash later on if there arent.
+            let old_move_times = move_times; 
+            if move_times > chart_vec[crate_from].len() {
+                move_times = chart_vec[crate_from].len() 
+            } 
+
+            if move_times != old_move_times {
+                println!("line {line:?} asks to move {old_move_times:?} crates to be moved but {move_times:?} crates will be moved from {crate_from:?} to {crate_to:?}");
+            } else {
+                println!("line {line:?} asks to move {move_times:?} crates from {crate_from:?} to {crate_to:?}");
+            }
+
+
+
+
+
+            // we save the var were about to remove to a buffer because this is a move not delete operation
+            let move_line = & mut chart_vec[crate_from];
+            let mut move_buffer = "".to_string();
+            for _ in move_buffer.chars() {
+                move_line.pop();
+            }
+            // the last crate is removed
+            if let Some(x) = chart_vec.get_mut(crate_from) {x.pop();}
+            // the move buffer than becomes is than saved to where it is moved to
+            let move_buffer_copy = &move_buffer.to_owned();
+
+            let response_push = ("pushed +{0} onto {1}", &move_buffer.clone(), &crate_to);
+
+            if let Some(x) = chart_vec.get_mut(crate_to) {x.push_str(&move_buffer); println!("{response_push:#?}")}
+            println!("moved crates {} from {crate_from:?} to {crate_to:?}", move_buffer_copy.len());
+
+            let chart_vec_crate_from = &chart_vec[crate_from].to_uppercase();
+            let chart_vec_crate_to = &chart_vec[crate_to].to_uppercase();
+
+            if chart_vec_crate_from == "" {
+                println!("line (removed from) now contains nothing")
+            } else {
+                println!("line (removed from) is now {chart_vec_crate_from}");
+            }
+
+            println!("line (added to) is now {chart_vec_crate_to}");
+
+
+
+
+
+
+        } 
+    }
+
+    let mut top = "".to_string();
+
+    for stack in &chart_vec {
+        if stack != "" {
+            if stack == " " {
+                top.push('|');
+            } else {
+                top.push(stack.chars().last().unwrap_or('/'));
+            }
+        }
+    }
+
+    let top = top.replace("/", "");
+    let top = top.replace("|", " ");
+    let top = top.to_uppercase();
+
+    print!("\nthe old crate chart looks like:\n");
+    println!("   1  2  3  4  5  6  7  8  9");
+
+    let mut index = 0;
+    for line in chart_vec_old {
+        index += 1;
+        let line = line.to_uppercase();
+        print!("{index} ");
+        for char in line.chars() {
+            print!("[{char}]");
+        }
+        println!();
+    }
+
+    let mut num_crates_one = 0;
+    for stack in chart_vec_old {
+        for var_crate in stack.chars() {
+            num_crates_one += 1;
+        }
+    }
+
+    print!("\nthe final crate chart looks like:\n");
+    println!("   1  2  3  4  5  6  7  8  9");
+
+    let mut index = 0;
+    for line in &chart_vec {
+        index += 1;
+        let line = line.to_uppercase();
+        print!("{index} ");
+        for char in line.chars() {
+            print!("[{char}]");
+        }
+        println!();
+    }
+
+    let mut num_crates_two = 0;
+    for stack in &chart_vec {
+        for var_crate_two in stack.chars() {
+            num_crates_two += 1;
+        }
+    }
+    if num_crates_one == num_crates_two {
+        println!("\nthe number of crates is the same ✅ ({num_crates_one})")
+    } else {
+        let error = "NOT the same ❎";
+        println!("\nthe number of crates is {} ({}, {})",error.coloriser(Colors::Red) , {num_crates_one}, {num_crates_two})
+    }
+
+    println!("the top crates are: {top}");
+}
