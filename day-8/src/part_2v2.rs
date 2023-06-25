6082fbad6999732470c77bf7d98ab793617d06d3
ostap timur tymchenko <ostap.tymchenko@gmail.com>
2023-06-25 09:35:06 -0400

    going to pride parade todat :) got to get readdy. the transition away from a string based system is going allright it should be done in a day or two.

--- day-8/src/part_2v2.rs
+++ day-8/src/part_2v2.rs
@@ -22,19 +22,29 @@ fn open_file(file_path: &str) -> String {
     data
 }
 
-fn parse(input_data:String) {
-    for line in input_data.split('\n').collect() {
-        line.chars().parse::<i32>().collect();
+fn parse(input_data:String) -> Vec<Vec<i8>> {
+    let mut parsed_forrest: Vec<Vec<i8>> = Vec::new();
+    for line in input_data.split('\n') {
+        // dbg!(line);
+        // forrest.push(vec![(line.chars().map(|c| c.to_digit(10).unwrap()).sum::<u32>() as i8)]);
+        let mut parsed_line:Vec<i8> = Vec::new();
+        for number in line.chars() {
+            parsed_line.push(number.to_digit(10).unwrap() as i8);
+        }
+        parsed_forrest.push(parsed_line);
     }
-    return 
+
+    dbg!(&parsed_forrest);
+    parsed_forrest
 } 
 
 pub fn main() {
     let forrest = parse(open_file("src/dummy-data.txt"));
+    // display_forrest(forrest, "Test forrest one");
 
     // let forrest: Vec<String> = file.split('\n').map(|x| x.to_string()).collect();
     
-    display_forrest(&forrest, "input"); 
+    // display_forrest(&forrest, "input"); 
 
     // for row in forrest {
     //     for considered_tree in row.chars() {
@@ -51,41 +61,40 @@ pub fn main() {
     //     }
     // }
     
-    for (x, row) in forrest.iter().enumerate() {
-        for (y, tree) in row.chars().enumerate() {
-            // println!("x:{x}, row:{row}, y:{y}, tree:{tree}")
-            // println!("row {x} column {y} is {}", check_height(x, y, &forrest));
+    // for (x, row) in forrest.iter().enumerate() {
+    //     for (y, tree) in row.chars().enumerate() {
+    //         // println!("x:{x}, row:{row}, y:{y}, tree:{tree}")
+    //         // println!("row {x} column {y} is {}", check_height(x, y, &forrest));
 
 
-            
-        }
-    }
+    //         
+    //     }
+    // }
 }
 
-fn calc_senic_score(x: usize, y:usize, forrest: &Vec<String>) {
-    let center_height = check_height(x, y, forrest);
-    let mut counter: usize = 0;
-    loop {
-        counter += 1;
-        if !check_height(x+counter, y+counter, forrest) >= center_height {
-            break;
-        }
-    }
-}
+// fn calc_senic_score(x: usize, y:usize, forrest: &Vec<String>) {
+//     let center_height = check_height(x, y, forrest);
+//     let mut counter: usize = 0;
+//     loop {
+//         counter += 1;
+//         if !check_height(x+counter, y+counter, forrest) >= center_height {
+//             break;
+//         }
+//     }
+// }
 
 fn check_height(x: usize, y:usize, forrest: &Vec<String>) -> char {
     forrest[x].chars().nth(y).unwrap()
 }
 
-fn display_forrest(forrest_reconstruction: &Vec<String>, name: &str) {
+fn display_forrest(forrest: Vec<Vec<i8>>, name: &str) {
     println!("\n | {name}");
-    for (row_iter, row) in forrest_reconstruction.iter().enumerate() {
-        if row_iter < 10 {
+    for row in forrest.iter() {
             // The different spacing is on pourpose in 
             // order to get a cleaner formated output
-            println!("{row_iter}|  {row}");
-        } else {
-            println!("{row_iter}| {row}");
-        }
+            // println!("{row_iter}|  {row}");
+            dbg!(row);
+            // println!("{row_iter}| {row}");
+            dbg!(row);
     }
 } 
