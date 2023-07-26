2d861134c607af5576564162b878e34d7c83d2fa
ostap timur tymchenko <ostap.tymchenko@gmail.com>
2023-07-25 21:55:03 -0400

    end of day

--- y2022/4-day/src/main.rs
+++ y2022/4-day/src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs, path::Path};
 use itertools::Itertools;
+use color_eyre::eyre::Result;
 
 fn read_data(data_path: &std::path::Path) -> String {
     fs::read_to_string(&data_path).expect("data parse fail")
@@ -7,18 +8,28 @@ fn read_data(data_path: &std::path::Path) -> String {
 
 fn part_1() {
     let data = read_data(Path::new("./data.txt"));
-    for line in data.lines() {
-        let ranges: (&str, &str) = line.split(',').into_iter().next_tuple().unwrap()
-            // for tomorow: potentially merge line 11 and 14. also auto turn to i32?
-        // let generated_iter = bounds.split('-').into_iter().next_tuple().unwrap();
-        // let range_1: (i32, i32) = ranges.0.split('-').into_iter().next_tuple().unwrap()
-        dbg!(range_1);
+    let fully_containded_pairs = 0;
 
-        panic!()
+    for line in data.lines() {
+        let (str_range_a, str_range_b) = line.split(',').collect_tuple().unwrap();
+
+        let (s0, e0) = str_range_a.split('-').collect_tuple::<(&str, &str)>().unwrap();
+        let (s1, e1) = str_range_b.split('-').collect_tuple::<(&str, &str)>().unwrap();
+
+        let range_a = s0.parse::<i32>()..s1.parse::<i32>();
+        let range_b = s0.parse::<i32>()..s1.parse::<i32>();
+
+
+
+        // dbg!(range_a, range_b);
+        panic!("MANUAL PANIC")
     }
 
 }
 
-pub fn main () {
-    part_1()
+pub fn main() -> Result<()> {
+    color_eyre::install()?;
+    part_1();
+
+    Ok(())
 }
--- y2022/5-day/src/main.rs
+++ y2022/5-day/src/main.rs
@@ -1,9 +1,43 @@
-// mod part_1;
-mod part_2;
-// mod test;
+use std::fs;
+use std::path::Path;
+
+fn read_data(data_path: &std::path::Path) -> String {
+    fs::read_to_string(&data_path).expect("data parse fail")
+}
+
+struct Data {
+    data: String,
+}
+
+struct Head {
+    data: String,
+    ends_at_line: i32,
+}
+
+struct Body {
+    data: String,
+}
+
+fn parse_head(data:Data) -> Head {
+    let mut head = String::from("");
+    for (line_num, line_data) in data.data.lines().enumerate() {
+        if line_data.is_empty() {
+            break;
+        } else {
+            head.push_str(line);
+            head.push('\n');
+        }
+    }
+    Head { data: head }
+}
+
+fn parse body
+
+fn part_one() {
+    let data = read_data(Path::new("./data.txt"));
+
+}
 
 fn main() {
-    // part_1::main();
-    part_2::main();
-    // test::main();
+    part_one();
 }
