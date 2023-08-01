0cd6ec6c0e43e061c7aa22e6b17a5d5830972493
ostap timur tymchenko <ostap.tymchenko@gmail.com>
2023-08-01 09:34:58 -0400

    switching to vec soon, + d7 save

--- y2022/7-day/src/main.rs
+++ y2022/7-day/src/main.rs
@@ -18,19 +18,19 @@ const WHITESPACE: char = ' ';
 const ROOT: &str = "/";
 
 #[derive(Clone, Default, Debug)]
-struct StackPath(Vec<String>);
+struct StackPath<'a> (Vec<&'a str>);
 
-impl StackPath {
-    fn push(&mut self, s: String) {
+impl StackPath<'_> {
+    fn push(&mut self, s: &str) {
         self.0.push(s);
     }
 
-    fn pop(&mut self) -> String {
+    fn pop(&mut self) -> &str {
         self.0.pop().expect("stack should not be empty")
     }
 }
 
-impl fmt::Display for StackPath {
+impl fmt::Display for StackPath<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         for s in self.0.iter() {
             write!(f, "[{s}]")?
@@ -40,9 +40,9 @@ impl fmt::Display for StackPath {
     }
 }
 
-fn go_to_target(mut path: StackPath, target: String) -> StackPath {
+fn go_to_target<'a> (mut path: StackPath<'a>, target: &'a str) -> StackPath<'a> {
     if target == ROOT {
-        return StackPath(vec![ROOT.to_string()]);
+        return StackPath::default();
     } else if target == ".." {
         StackPath(vec![path.pop()])
     } else {
@@ -51,28 +51,37 @@ fn go_to_target(mut path: StackPath, target: String) -> StackPath {
     }
 }
 
-fn get_top_lvl_dir(path: &StackPath) -> String {
-    let binding = path.0.get(0).expect("get_top_lvl_dir should be ran from a dir other than root");
-    binding.to_string()
+//  DEPRECTADED
+// fn get_top_lvl_dir(path: &StackPath) -> Option<String> {
+//     let binding = &path.0.first()?;
+//
+//     return Some(binding.to_string());
+// }
+
+fn add_my_size_to_all_my_parents(path: StackPath, size: i32, index: HashMap<String, i32>) {
+    for dir in path.0 {
+        dbg!(dir);
+    }
 }
 
 fn part_one(file_name: &str) -> i32 {
     let data_path = "data/".to_owned() + file_name;
     let data = read_data(Path::new(&data_path));
 
-    let mut sim_path = StackPath::default();
-    let mut top_lvl_dir_index: HashMap<String, i32> = HashMap::new();
+    let mut sim_path = StackPath(vec!["/"]);
+    let mut dir_size_index: HashMap<String, i32> = HashMap::new();
+    dir_size_index.insert(String::from("/"), 0);
 
     for line in data.lines() {
         let fc = line.chars().nth(0).expect("line should have at least one char");
 
         if fc == '$' {
-            // command
+            // comman0d
             let sc = line.chars().nth(2).expect("line command should have at least two chars");
             if sc == 'c' {
                 // command starting with c is cd
                 print!("goto from '{sim_path}' with target '{}'", line.split_whitespace().nth(2).unwrap());
-                let target = line.split_whitespace().nth(2).expect("line with command should have at least 3 words").to_string();
+                let target = line.split_whitespace().nth(2).expect("line with command should have at least 3 words");
                 sim_path = go_to_target(sim_path, target);
                 println!(" new path:'{sim_path:?}'");
 
@@ -86,27 +95,24 @@ fn part_one(file_name: &str) -> i32 {
         } else {
             // file
             let file_size = line.split_once(WHITESPACE).expect("file line should have space").0.parse::<i32>().expect("file size should be parsable");
-            
-            if file_size <= 100_000 {
-                let dir = get_top_lvl_dir(&sim_path);
-                // if top_lvl_dir_index.contains_key(&dir) {
-                //     let top_lvl_dir_index.get_mut(&dir)
-                // }
-                if let Some(v) = top_lvl_dir_index.get_mut(&dir) {
-                    *v + file_size;
-                } else {
-                    top_lvl_dir_index.insert(dir, file_size);
-                };
-            }
+            let where_am_i = format!("get top lvl dir called from {}, with fs {}", sim_path, file_size);
+
+            // if let Some(top_lvl_dir) = get_top_lvl_dir(&sim_path) {
+            //     println!("sucess, {where_am_i}");
+            //
+            //     if let Some(v) = top_lvl_dir_index.get_mut(&top_lvl_dir) {
+            //         *v = *v + file_size;
+            //     } else {
+            //         top_lvl_dir_index.insert(top_lvl_dir, file_size);
+            //     };
+            // } else {
+            //     println!("failure, {where_am_i}");
+            // }
+
         }
     }
 
-    let mut total_added_size = 0;
-    for value in top_lvl_dir_index.values() {
-        total_added_size += value;
-    }
-
-    total_added_size
+    todo!()
 }
 
 #[cfg(test)]
