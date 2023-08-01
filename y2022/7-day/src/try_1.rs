1ab84912ea600dce09dab6f9e2c983664bc70aeb
ostap timur tymchenko <ostap.tymchenko@gmail.com>
2023-07-31 23:28:41 -0400

    Minimal viable state for day 7 p1

--- y2022/7-day/src/try_1.rs
+++ y2022/7-day/src/try_1.rs
@@ -17,7 +17,7 @@ const FOLDER_SPLIT_CHAR: char = '/';
 const WHITESPACE: char = ' ';
 const ROOT: &str = "/";
 
-#[derive(Clone, Default, Debug)]
+#[derive(Clone, Default)]
 struct StackPath(Vec<String>);
 
 impl StackPath {
@@ -33,7 +33,7 @@ impl StackPath {
 impl fmt::Display for StackPath {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         for s in self.0.iter() {
-            write!(f, "[{s}]")?
+            write!(f, "{s}")?
         }
 
         Ok(())
@@ -46,67 +46,56 @@ fn go_to_target(mut path: StackPath, target: String) -> StackPath {
     } else if target == ".." {
         StackPath(vec![path.pop()])
     } else {
+        // return StackPath(vec![path.0[0].to_owned() + target + FOLDER_SPLIT_STR]);
         path.push(target);
         path
     }
 }
 
-fn get_top_lvl_dir(path: &StackPath) -> String {
-    let binding = path.0.get(0).expect("get_top_lvl_dir should be ran from a dir other than root");
-    binding.to_string()
+// fn get_top_lvl_dir(path: &str ) -> &str {
+//     path.split_once(FOLDER_SPLIT_CHAR).expect("get_top_lvl_dir should never be called on root folder. Call it in a dir bellow root").0
+// }
+
+fn get_top_lvl_dir(path: StackPath) -> String {
+    path.0.get(0).expect("get_top_lvl_dir should be ran from a dir other than root").to_string()
 }
 
 fn part_one(file_name: &str) -> i32 {
     let data_path = "data/".to_owned() + file_name;
     let data = read_data(Path::new(&data_path));
 
+    // let mut sim_path.0: Vec<&str> = vec![];
     let mut sim_path = StackPath::default();
-    let mut top_lvl_dir_index: HashMap<String, i32> = HashMap::new();
+    let mut top_lvl_dir_size: HashMap<String, i32> = HashMap::new();
 
     for line in data.lines() {
-        let fc = line.chars().nth(0).expect("line should have at least one char");
-
-        if fc == '$' {
-            // command
-            let sc = line.chars().nth(2).expect("line command should have at least two chars");
-            if sc == 'c' {
-                // command starting with c is cd
+        let fw = line.split_at(1); // get the first word
+        let fc = fw.0; // get the first char
+        if fc == "$" {
+            // if fw.1.split_whitespace().next().expect("line with command should have whitespace") == "cd" {
+            let first_command_c = line.chars().nth(2).expect("line with command should have at least 3 chars");
+            if first_command_c == 'c' {
                 print!("goto from '{sim_path}' with target '{}'", line.split_whitespace().nth(2).unwrap());
                 let target = line.split_whitespace().nth(2).expect("line with command should have at least 3 words").to_string();
                 sim_path = go_to_target(sim_path, target);
-                println!(" new path:'{sim_path:?}'");
-
-            } else if sc == 'l' {
-                // command starting with l is ls
+                println!(" new path:'{sim_path}'")
+            } else if first_command_c == 'l' {
+                // println!("ls.. found but done use me!");
             } else {
                 panic!("line with command should have at least 3 chars");
             }
-        } else if fc == 'd' {
-            // dir name
+        } else if fc == "d" {
+            // shows dir name
+            // should never be used
         } else {
-            // file
+            // shows files and sized
             let file_size = line.split_once(WHITESPACE).expect("file line should have space").0.parse::<i32>().expect("file size should be parsable");
-            
             if file_size <= 100_000 {
-                let dir = get_top_lvl_dir(&sim_path);
-                // if top_lvl_dir_index.contains_key(&dir) {
-                //     let top_lvl_dir_index.get_mut(&dir)
-                // }
-                if let Some(v) = top_lvl_dir_index.get_mut(&dir) {
-                    *v + file_size;
-                } else {
-                    top_lvl_dir_index.insert(dir, file_size);
-                };
+                top_lvl_dir_size.insert(get_top_lvl_dir(sim_path), file_size);
             }
         }
     }
-
-    let mut total_added_size = 0;
-    for value in top_lvl_dir_index.values() {
-        total_added_size += value;
-    }
-
-    total_added_size
+    0
 }
 
 #[cfg(test)]
